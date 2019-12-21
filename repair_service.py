1199faf5399a7c971c35c78d21cf6bf946b6ec33
Andrew Yesipcuk <yesipchuk.andriy@gmail.com>
2019-12-21 22:35:59 +0200

    some service and api improvements

--- repair_service.py
+++ repair_service.py
@@ -11,3 +11,30 @@ class RepairService:
         Device(5, "MacBook Air", "TouchID does not work"),
         Device(6, "iPhone 11 Pro Max Plus S", "Dust on camera")
     ]
+
+    def get_devices(self):
+        return self.devices_on_repair
+
+    def get_device_by_id(self, id):
+        return list(filter(lambda d: d.id == id, self.devices_on_repair))
+
+    def add_device(self, name, issue):
+        new_device_id = self.devices_on_repair[-1].id + 1
+        new_device = Device(new_device_id, name, issue)
+        self.devices_on_repair.append(new_device)
+        return new_device
+
+    def update_device(self, id, name, issue):
+        device_update = self.get_device_by_id(id)
+        if len(device_update) == 0:
+            return device_update
+        device_update[0].name = name
+        device_update[0].issue = issue
+        return device_update
+
+    def delete_device(self, id):
+        device_for_delete = list(filter(lambda d: d.id == id, self.devices_on_repair))
+        if len(device_for_delete) != 0:
+            return False
+        self.devices_on_repair.remove(device_for_delete[0])
+        return True
